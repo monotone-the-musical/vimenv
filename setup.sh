a53bbcc51e6eb856c5d715eba284708710041962
smackbanger <smackbanger@gmail.com>
2016-07-17 14:30:53 +1000

    bla

--- setup.sh
+++ setup.sh
@@ -13,8 +13,16 @@ for a in `ls -1 *.tgz`; do tar -zxvf $a; done
 
 rm -f *.tgz
 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
-echo ; echo
-echo "Setup complete. Re login then from vim run :PlugInstall"
-echo "Then run setup2.sh"
+~/.fzf/install
+
+cp dotfile.fzf.bash ~/.fzf.bash
+
+cp dotfile.fzf-marks.plugin.bash ~/.fzf-marks.plugin.bash
+
+echo ; echo ; echo
+echo "-------------------------------------------------------------------------------------------" ; echo
+echo "Setup complete. Re login then from vim run and run :PlugInstall to install any plugins."
+echo ; echo "-------------------------------------------------------------------------------------------" 
 echo ; echo
--- setup2.sh
+++
@@ -1,6 +0,0 @@
-
-cp dotfile.fzf.bash ~/.fzf.bash
-
-cp dotfile.fzf-marks.plugin.bash ~/.fzf-marks.plugin.bash
-
-echo ; echo "All done. Re login and setup will be complete." ; echo
