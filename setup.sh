09d79c4b1f7621320ada976cf93675ba56915cf6
smackbanger <smackbanger@gmail.com>
2016-07-17 12:59:04 +1000

    bla

--- setup.sh
+++ setup.sh
@@ -3,18 +3,18 @@ cp -R .vim ~/
 
 cp .vimrc ~/
 
-cp pre_plugged/* ~/.vim/plugged/
+mkdir -p ~/.vim/plugged/
+
+cp ./pre_plugged/* ~/.vim/plugged/
 
 cd ~/.vim/plugged/
 
-tar xvf *.tgz
+for a in `ls -1 *.tgz`; do tar -zxvf $a; done
 
 rm -f *.tgz
 
-cp dotfile.fzf.bash ~/.fzf.bash
-
-cp dotfile.fzf-marks.plugin.bash ~/.fzf-marks.plugin.bash
 
 echo ; echo
 echo "Setup complete. Re login then from vim run :PlugInstall"
+echo "Then run setup2.sh"
 echo ; echo
---
+++ setup2.sh
@@ -0,0 +1,6 @@
+
+cp dotfile.fzf.bash ~/.fzf.bash
+
+cp dotfile.fzf-marks.plugin.bash ~/.fzf-marks.plugin.bash
+
+echo ; echo "All done. Re login and setup will be complete." ; echo
