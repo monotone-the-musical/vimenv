d77ad3b9c65f4aa3f44d891ccbfdb3518a7c8e0a
tsdacc <tsdacc@mymemowork.com>
2016-07-18 15:52:26 +1000

    bla

--- setup.sh
+++ setup.sh
@@ -23,8 +23,30 @@ cp dotfile.fzf.bash ~/.fzf.bash
 
 cp dotfile.fzf-marks.plugin.bash ~/.fzf-marks.plugin.bash
 
+echo "Set color mode to 256? (y|N):"
+read userin
+if [ "$userin" != "y" ]
+then
+  echo "not setting"
+else
+  echo "export TERM=xterm-256color" >> ~/.bashrc
+  echo ; echo "256 color set" ; echo ; echo
+fi
+
+echo "Set zenburn to default color-scheme? (y|N):"
+read userin
+if [ "$userin" != "y" ]
+then
+  echo "not setting"
+else
+  echo "colorscheme zenburn" >> ~/.vimrc
+  echo ; echo ; echo "zenburn set as default"
+  destdir=`find /usr/share -type f -name peachpuff.vim | sed s/"peachpuff.vim"/""/g`
+  echo ; echo "recomend copying zenburn to $destdir"
+fi
 echo ; echo ; echo
 echo "-------------------------------------------------------------------------------------------" ; echo
 echo "Setup complete. Re login then from vim run and run :PlugInstall to install any plugins."
-echo ; echo "-------------------------------------------------------------------------------------------" 
+echo ; echo "-------------------------------------------------------------------------------------------"
 echo ; echo
+
