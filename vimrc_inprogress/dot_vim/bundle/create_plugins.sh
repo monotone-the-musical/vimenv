74126ad958d44d443cb042cbeac37cfeb110e322
smackbanger <smackbanger@gmail.com>
2018-08-19 18:51:51 +1000

    auto commit from home macro

--- vimrc_inprogress/dot_vim/bundle/create_plugins.sh
+++ vimrc_inprogress/dot_vim/bundle/create_plugins.sh
@@ -1,5 +1,6 @@
 
-git clone https://github.com/vim-scripts/AutoComplPop.git
+git clone https://github.com/othree/vim-autocomplpop.git
+git clone https://github.com/clones/vim-l9.git
 git clone https://github.com/vim-scripts/goyo.vim.git
 git clone https://github.com/vim-scripts/mru.vim.git
 git clone https://github.com/scrooloose/nerdtree.git
