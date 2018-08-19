e0cafb05428e0f4c98a08c260cc7e8940b53bed1
smackbanger <smackbanger@gmail.com>
2018-08-19 19:02:04 +1000

    bla

---
+++ vimrc_inprogress/dot_vim_downloaded/bundle/create_plugins.sh
@@ -0,0 +1,8 @@
+
+git clone https://github.com/othree/vim-autocomplpop.git
+git clone https://github.com/clones/vim-l9.git
+git clone https://github.com/vim-scripts/goyo.vim.git
+git clone https://github.com/vim-scripts/mru.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/tpope/vim-fugitive.git
+
