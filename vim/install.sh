4c89ee3698c6973210c6b524d9f6efd0b060bccf
tsdacc <tsdacc@mymemowork.com>
2018-08-21 09:33:12 +1000

    auto commit at 20180821

--- vim/install.sh
+++ vim/install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-backupdir="~/.vim_backup.`date +%Y%m%d`"
+backupdir=".vim_backup.`date +%Y%m%d`"
 
-mkdir -p ${backupdir}
+mkdir -p ~/${backupdir}
 
-mv ~/.vim ${backupdir}
+mv ~/.vim ~/${backupdir}
 
-mv ~/.vimrc ${backupdir}
+mv ~/.vimrc ~/${backupdir}
 
 cp -R -P ./dot_vim_downloaded ~/.vim 
 
 cp ./dot_vimrc ~/.vimrc
 
-echo ; echo "vim environment setup. Original environment backed up to ${backupdir}" ; echo
+echo ; echo "vim environment setup. Original environment backed up to ~/${backupdir}" ; echo
 
 exit
