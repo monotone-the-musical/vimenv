1d754828e37748e86756a97013316eef3f4df7a0
smackbanger <smackbanger@gmail.com>
2018-08-27 02:11:24 +1000

    auto commit from home macro

--- vim/install.sh
+++ vim/install.sh
@@ -18,7 +18,7 @@ cp -p -R ./dot_vim_downloaded ~/.vim
 cp ./dot_vimrc ~/.vimrc
 
 # restore views
-cp -p -R ~/${backupdir}/view ~/.vim/
+cp -p -R ~/${backupdir}/.vim/view ~/.vim/
 
 echo ; echo "vim environment setup. Original environment backed up to ~/${backupdir}" ; echo
 
